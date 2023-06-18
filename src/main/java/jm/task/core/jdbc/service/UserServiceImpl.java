3dbb7d572609370c5d740ec51085a9e15d195c54
Maksim <kevokk@gmail.com>
2023-06-18 20:18:33 +0300

    added new hibernate service

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -7,30 +8,51 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private static final UserDaoJDBCImpl USER_DAO_JDBC = new UserDaoJDBCImpl();
-
+    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
     public void createUsersTable() {
-        USER_DAO_JDBC.createUsersTable();
+        userDaoHibernate.createUsersTable();
     }
 
     public void dropUsersTable() {
-        USER_DAO_JDBC.dropUsersTable();
+        userDaoHibernate.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        USER_DAO_JDBC.saveUser(name, lastName, age);
+        userDaoHibernate.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        USER_DAO_JDBC.removeUserById(id);
+        userDaoHibernate.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return USER_DAO_JDBC.getAllUsers();
+        return userDaoHibernate.getAllUsers();
     }
 
     public void cleanUsersTable() {
+        userDaoHibernate.cleanUsersTable();
+    }
+
+    /*
+    private static final UserDaoJDBCImpl USER_DAO_JDBC = new UserDaoJDBCImpl();
+    public void createUsersTable() {
+        USER_DAO_JDBC.createUsersTable();
+    }
+    public void dropUsersTable() {
+        USER_DAO_JDBC.dropUsersTable();
+    }
+    public void saveUser(String name, String lastName, byte age) {
+        USER_DAO_JDBC.saveUser(name, lastName, age);
+    }
+    public void removeUserById(long id) {
+        USER_DAO_JDBC.removeUserById(id);
+    }
+    public List<User> getAllUsers() {
+        return USER_DAO_JDBC.getAllUsers();
+    }
+    public void cleanUsersTable() {
         USER_DAO_JDBC.cleanUsersTable();
     }
+     */
 
 }

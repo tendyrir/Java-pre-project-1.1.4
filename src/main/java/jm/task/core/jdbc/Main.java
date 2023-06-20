e6375954b176ba4e3031e81080fd83915ead50e0
Maksim <kevokk@gmail.com>
2023-06-20 18:04:40 +0300

    1.1.4 fix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.util.List;
@@ -9,24 +10,24 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        UserServiceImpl usi = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
 
 //        Создание таблицы User(ов)
-        usi.createUsersTable();
+        userService.createUsersTable();
 //        Добавление 4 User(ов) в таблицу с данными на свой выбор.
 //        После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
-        usi.saveUser("Stepan", "Shatalov", (byte) 23);
-        usi.saveUser("Ivan", "Efimov", (byte) 24);
-        usi.saveUser("Natasha", "Vasenkova", (byte) 26);
-        usi.saveUser("Maxim", "Kuznetsov", (byte) 25);
+        userService.saveUser("Stepan", "Shatalov", (byte) 23);
+        userService.saveUser("Ivan", "Efimov", (byte) 24);
+        userService.saveUser("Natasha", "Vasenkova", (byte) 26);
+        userService.saveUser("Maxim", "Kuznetsov", (byte) 25);
 //        Получение всех User из базы и вывод в консоль (должен быть переопределен toString в классе User)
-        List<User> userList = usi.getAllUsers();
-        for (User user: userList) {
+        List<User> userList = userService.getAllUsers();
+        for (User user : userList) {
             System.out.println(user.toString());
         }
 //        Очистка таблицы User(ов)
-        usi.cleanUsersTable();
+        userService.cleanUsersTable();
 //        Удаление таблицы
-        usi.dropUsersTable();
+        userService.dropUsersTable();
     }
 }
